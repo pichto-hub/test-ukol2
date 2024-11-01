e0ca8d1ba79bd26c2c33670a50b00b531f02a1d9
pichlik <pichto@ms.spsostrov.cz>
2024-11-01 11:46:40 +0100

    Opravená chyba v souboru skakalpes.php

--- skakalpes.php
+++ skakalpes.php
@@ -1,9 +1,8 @@
 #!/usr/bin/env php
 <?php
-
 echo "Skákal pes,\n";
 echo "přes oves,\n";
-echo "přes zelenou louku.\n"
+echo "přes zelenou louku.\n";
 echo "šel za ním\n";
 echo "myslivec,\n";
 echo "péro na klobouku.\n";
